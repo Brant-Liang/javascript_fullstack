437e50f3c91d430bc6fd6e9c987c70779c1808e4
Brant-Liang <1351805238@qq.com>
2019-10-19 16:35:34 +0800

    零食购物

--- JS/arithmetic/homework3.17.js
+++ JS/arithmetic/homework3.17.js
@@ -1,4 +1,6 @@
-
+//利用数学归纳法找到转移方程
+//动态方法的转移方程：
+// triangle[i][j] = Math.min(triangle[i+1][j],triangle[i+1][j+1])+triangle[i][j];
 const minmimTotal = function(triangle){
    for(let i =triangle.length - 2; i >= 0; i--){
       for(let j = 0;j <= i; j++){
