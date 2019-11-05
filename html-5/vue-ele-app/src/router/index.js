e753271f773deca1a0fe8ab7439a861e546f004b
Brant-Liang <1351805238@qq.com>
2019-11-05 21:21:36 +0800

    饿了么商品页面菜单

--- html-5/vue-ele-app/src/router/index.js
+++ html-5/vue-ele-app/src/router/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
+import Goods from '@/components/goods/goods'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/goods',
+      name: 'goods',
+      component: Goods
+    }
     // {
     //   path: '/',
     //   name: 'HelloWorld',
