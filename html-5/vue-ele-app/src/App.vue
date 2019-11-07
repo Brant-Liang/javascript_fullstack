b79d97911a202472fd34974f1900afdde9a7e27a
Brant-Liang <1351805238@qq.com>
2019-11-07 23:43:25 +0800

    eleme vue develope

--- html-5/vue-ele-app/src/App.vue
+++ html-5/vue-ele-app/src/App.vue
@@ -13,12 +13,14 @@
         <router-link to="seller">商家</router-link>
       </div>
     </div>
+    
     <router-view/>
   </div>
 </template>
 
 <script>
 import header from '@/components/header/header.vue'
+
 export default {
   name: 'App',
   data () {
@@ -27,7 +29,7 @@ export default {
     }
   },
   components: {
-    'v-header': header
+    'v-header': header,
   },
   created () {
     this.$http.get('http://localhost:8080/static/seller.json', {})
--- html-5/vue-ele-app/src/components/goods/goods.vue
+++ html-5/vue-ele-app/src/components/goods/goods.vue
@@ -11,21 +11,52 @@
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper"></div>
+      <div class="foods-wrapper" ref="foodsWrapper">
+        <ul>
+          <li class="food-list" v-for="(item,index) in goods" :key="index" ref="foodList">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li class="food-item border-1px"  v-for="(food,index) in item.foods" :key="index" >
+                <div class="icon">
+                  <img :src="food.icon" alt="">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
+    <shopcart></shopcart>
   </div>
 </template>
 
 <script>
 import BScroll from 'better-scroll'
+import shopcart from '@/components/shopcart/shopcart.vue'
 export default {
   name: 'Goods',
   data () {
     return {
       classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
       goods: {},
-      curIndex: 0
+      listHeight: [],
+      scrollY: 0
     }
+  },
+  components: {
+    'shopcart': shopcart
   },
   created () {
     this.$http.get('http://localhost:8080/static/goods.json')
@@ -34,19 +65,52 @@ export default {
         if(res.data.errno == 0){
         this.goods =  res.data.data
         this.$nextTick(()=>{ //在html已经渲染完成之后执行
-        this._initScroll() 
+            this._initScroll() 
+            this._caculateHeight()
         })
       }
     })
   },
+  computed: {
+    curIndex () {
+      this.listHeight.forEach((item, index) => {
+        if(!this.listHeight[index + 1] || (this.scrollY >= this.listHeight[index] && this.scrollY < this.listHeight[index + 1])){
+          return index;
+        }
+      })
+      return 0;
+    }
+  },
   methods: {
     _initScroll () {
-      this.menuScroll = new BScroll(this.$refs.menuWrapper,{
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {
         click: true
+      }),
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        click: true,
+        probeType: 3
+      }),
+      this.foodsScroll.on("scroll", pos => {
+        this.scrollY = Math.abs(Math.round(pos.y))
+        console.log(this.scrollY)
       })
     },
     selectMenu (idx) {
       this.curIndex = idx
+      let foodList = this.$refs.foodList
+      let el = foodList[idx]
+      this.foodsScroll.scrollToElement(el, 300)
+    },
+    _caculateHeight () {
+      let foodList = this.$refs.foodList
+      let height = 0
+      this.listHeight.push(height)
+      for (let i in foodList) {
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+      console.log(this.listHeight)
     }
   }
 }
@@ -79,7 +143,7 @@ export default {
         font-weight 700
         .text
           border-none()
-      .icon 
+      .icon
         display inline-block
         vertical-align top
         width 12px
@@ -97,11 +161,67 @@ export default {
           bg-image('invoice_3')
         &.special
           bg-image('special_3')
-      .text 
+      .text
         display table-cell
         width 56px
         vertical-align middle
         border-1px(rgba(7, 17, 27, 0.1))
         font-size 12px
-      
+  .foods-wrapper
+    flex 1
+    //overflow hidden
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147, 153, 159)
+      background #f3f5f7
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        border-none()
+        margin-bottom 0
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+        img
+          width 100%
+      .content
+        flex 1
+        .name
+          margin 2px 0 8px 0
+          height 14px
+          line-height 14px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .desc, .extra
+          line-height 10px
+          font-size 10px
+          color rgb(147, 153, 159)
+        .desc
+          line-height 12px
+          margin-bottom 8px
+        .extra
+          .count
+            margin-right 12px
+        .price
+          font-weight 700
+          line-height 24px
+          .now
+            margin-right 8px
+            font-size 14px
+            color rgb(240, 20, 20)
+          .old
+            text-decoration line-through
+            font-size 10px
+            color rgb(147, 153, 159)
+        .cartcontrol-wrapper
+          position absolute
+          right 0
+          bottom 12px
 </style>
