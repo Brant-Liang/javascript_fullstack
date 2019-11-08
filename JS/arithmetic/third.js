80272cb4107bc0dfc1ebf3c3deffdaa1bf826290
Brant-Liang <1351805238@qq.com>
2019-11-08 21:23:17 +0800

    函数式编程和面向对象继承

--- JS/arithmetic/third.js
+++ JS/arithmetic/third.js
@@ -1,16 +1,22 @@
+//即按高位到低位的顺序搜索
+//若各位数字递减，则删除最后一个数字，否则删除第一个递增区间的首字符
 function getMaxNum(str, s){ 
-  var numLen = str.length - s;
-  var arr = []; //储存字符串
-  var maxNumArr = []; //储存输出的最大数
-  for(let i = 0; i < s + 1; i++){ //找倒数第s + 1及之前的最大数，先把 1 ~ s + 1 中的数储存到数组中
-    arr.push(str[i]);
+  var numLen = str.length - s; //找到 位数为numLen的最大数
+  var arr=[]; //对字符串进行删除操作
+  for(let i = 0; i < str.length; i++){ 
+    if(str[i] < str[i + 1]){
+      arr.push(str[i]);
+      s--;
+    }
+    for(let j = 0; j <= arr.length; j++){
+      str = str.replace(arr[j], "");
+    }
+    if(s <= 0){
+      return str;
+    }
   }
-  arr.sort((a, b) => b - a);  //降序排序
-  var max = arr[0];           //取最大值
-  maxNumArr.push(max);        //把最大值放在最左边
-  var maxIdx = str.indexOf(max); //找到可取的最大值在字符串中的下标
-  numLen --;
-  return str.substring(maxIdx + 1, str.length) // 2 - 7
+  
+  return str;
 }
 var myStr = '178536498762';
 var mS = 8;  //  12 - 4 = 8 
