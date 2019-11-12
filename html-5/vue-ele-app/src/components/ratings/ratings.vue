33b670d3442322268b47ea855b4832c59141849f
Brant-Liang <1351805238@qq.com>
2019-11-12 18:45:10 +0800

    饿了么商品页

---
+++ html-5/vue-ele-app/src/components/ratings/ratings.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1>4.2</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家69.5%</div>
+        </div>
+        <div class="overview-right"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Ratings'
+}
+</script>
+
+<style>
+
+</style>
