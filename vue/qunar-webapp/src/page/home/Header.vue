96e55219840c92ffc48f4e15dea4a0a8c86cad52
Brant-Liang <1351805238@qq.com>
2019-11-14 17:10:42 +0800

    qunar

---
+++ vue/qunar-webapp/src/page/home/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div class="header-l"></div>
+    <div class="header-input">
+      <input type="text" name="" id="">
+    </div>
+    <div class="header-right">
+      {{this.city}}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Header from './Header'
+export default {
+  data() {
+    return {
+      city: 'Beijin'
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
