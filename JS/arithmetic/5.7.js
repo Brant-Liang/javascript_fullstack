6b12d99207b77aaf097aa264187f5bceb83b0abd
Brant-Liang <1351805238@qq.com>
2019-11-17 13:33:47 +0800

    arithmetic

--- JS/arithmetic/5.7.js
+++ JS/arithmetic/5.7.js
@@ -4,26 +4,28 @@
  */ 
 var cost = 0;
 var x = [];
-var myC = [[1,2,3], [4, 5, 6],[7, 8, 9]];
+var myC = [[1,3,4], [4, 5, 6],[1, 7, 8]];
+var n = myC.length
 
-for(let i = 0; i < myC.length; i++){
+for(let i = 0; i < n; i++){
   cost += myC[i][i]     //cost : 第i个人分配第i件工作总成本
 }
-function work(i, count, C){
+function work(i, count){
   if(i > myC.length && count < cost ){
       cost = count;
       return ;
   }
   if(count < cost){
-      for(var j = 0; j < myC.length; j++){
+      for(var j = 0; j < n; j++){
           if(x[j] === 0){
               x[j] = 1;
-              work(i + 1, count + C[i][j], C);
+              work(i + 1, count + C[i][j]);
               x[j] = 0;
           }
       }
   }
-  return cost
+  
 }
 //myC[i][j] i代表第几个人 j代表每件事在不同人手中的费用
-console.log(work(1, 0, myC))
+work(1, 0, myC)
+console.log(10);
