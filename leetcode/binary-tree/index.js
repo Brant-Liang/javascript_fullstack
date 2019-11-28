1d67095abab44be10282d6b38efad0cd657d8bde
Brant-Liang <1351805238@qq.com>
2019-11-28 20:12:35 +0800

    binary tree order

---
+++ leetcode/binary-tree/index.js
@@ -0,0 +1,81 @@
+var tree = {
+  value: 1,
+  left: {
+    value: 2,
+    left: {
+      value: 4
+    }
+  },
+  right: {
+    value: 3,
+    left: {
+      value: 5,
+      left: {
+        value: 7
+      },
+      right: {
+        value: 8
+      }
+    },
+    right: {
+      value: 6
+    }
+  }
+}
+function preOrder (Obj) {
+  if(Obj){
+    console.log(Obj.value);
+    preOrder(Obj.left)
+    preOrder(Obj.right)
+  }
+}
+function inOrder (Obj) {
+  if(Obj) {
+    inOrder(Obj.left)
+    console.log(Obj.value);
+    inOrder(Obj.right)
+  }
+}
+function postOrder (Obj) {
+  if(obj) {
+    inOrder(Obj.left)
+    inOrder(Obj.right)
+    console.log(Obj.value);
+  }
+}
+//preOrder(tree)
+//inOrder(tree)
+
+// 深度优先 非递归
+var preOrderUnRecur = function (node) {
+  if(!node) {
+    throw new Error('Empty Tree!')
+  }
+  var stack = []
+  stack.push(node)
+  while(stack.length !== 0){
+    node = stack.pop()
+    console.log(node.value);
+    if(node.right) {
+      stack.push(node.right)
+    }
+    if(node.left){
+      stack.push(node.left)
+    }
+  }
+}
+//preOrderUnRecur(tree)
+
+// 广度优先
+
+function BreadthFirstUnRecur (biTree) {
+  let queue = []
+  queue.push(biTree)
+  while(queue.length !== 0) {
+    let node = queue.shift()
+    console.log(node.value);
+    if(node.left) queue.push(node.left)
+    if(node.right) queue.push(node.right)
+  }
+}
+BreadthFirstUnRecur(tree)
