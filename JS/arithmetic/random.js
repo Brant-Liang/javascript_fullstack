13045d961fa0b44bc935e82819ca95347e8a58c1
Brant-Liang <1351805238@qq.com>
2019-12-12 19:11:05 +0800

    css-calendor

---
+++ JS/arithmetic/random.js
@@ -0,0 +1,13 @@
+
+function getNums() {
+  let arr = [];
+  for (let i = 0; i < 4; i++) {
+    arr.push(Math.ceil(Math.random() * 9))
+  }
+  return arr
+}
+console.log(getNums());
+function process(arr) {
+  for(let i = 0; i < 4; i++) {
+  }
+}
