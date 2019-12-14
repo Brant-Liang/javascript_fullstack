a7a67ba05ea525833953664c324e45775fbd6daa
Brant-Liang <1351805238@qq.com>
2019-12-14 23:36:16 +0800

    vue-learn

---
+++ vue/mpvue/eleme/src/pages/goods/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="goods">
+    goods1
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .goods {
+    width: 100px;
+    height: 100px;
+  }
+</style>
