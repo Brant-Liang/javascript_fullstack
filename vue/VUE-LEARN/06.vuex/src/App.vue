e55e12ead43d8b2b37386f246e5918501e900a9d
Brant-Liang <1351805238@qq.com>
2020-02-06 01:31:56 +0800

    vuex start

---
+++ vue/VUE-LEARN/06.vuex/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h2>--------app内容--------</h2>
+    <h2>{{message}}</h2>
+    <h2>{{$store.state.count}}</h2>
+    <button @click="count++">+</button>
+    <button @click="count--">-</button>
+    <h2>-------helloVuex内容--------</h2>
+    <hello-vuex :count="count"/>
+  </div>
+</template>
+
+<script>
+import HelloVuex from './components/HelloVuex'
+export default {
+  name: 'App',
+  components: {
+    HelloVuex
+  },
+  data() {
+    return {
+      message: '你好啊！！！',
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ vue/VUE-LEARN/06.vuex/src/components/HelloVuex.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h2>{{$store.state.count}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    count: Number
+  },
+  data () {
+    return {
+      msg: 'Welcome to Vuex!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
