5d037141c864a8351578984a90c6fd10fab57607
Brant-Liang <1351805238@qq.com>
2020-03-03 23:41:24 +0800

    flex-container

---
+++ react/learn/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() { // jsx
+    return <div>
+            <button>hello react</button>
+           </div>
+  }
+
+}
+
+export default App
