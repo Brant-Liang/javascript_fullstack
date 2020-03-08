d4ca623993bbdb9093747acdbee1bec52a764f7a
Brant-Liang <1351805238@qq.com>
2020-03-08 17:57:36 +0800

    击鼓传花

--- JS/Data Structure/2.queue/queue.js
+++ JS/Data Structure/2.queue/queue.js
@@ -34,18 +34,41 @@ function Queue() {
 //使用对列
 var queue = new Queue()
 
-queue.enqueue(10)
-queue.enqueue(20)
-queue.enqueue(30)
-queue.enqueue(40)
+// queue.enqueue(10)
+// queue.enqueue(20)
+// queue.enqueue(30)
+// queue.enqueue(40)
 
-console.log(queue)
-queue.dequeue()
-console.log(queue)
-console.log(queue.front())
-console.log(queue.size())
-console.log(queue.toString());
-queue.dequeue()
-queue.dequeue()
-queue.dequeue()
-console.log(queue.isEmpty())
+// console.log(queue)
+// queue.dequeue()
+// console.log(queue)
+// console.log(queue.front())
+// console.log(queue.size())
+// console.log(queue.toString());
+// queue.dequeue()
+// queue.dequeue()
+// queue.dequeue()
+// console.log(queue.isEmpty())
+
+//击鼓传花
+function passGame(nameList, num) {
+  //1、创建一个对列
+  var myQueue = new Queue()
+  //2、将所有人依次加入到对列
+  for (let i = 0; i < nameList.length; i++) {
+    myQueue.enqueue(nameList[i])
+  }
+  console.log(myQueue.size());
+  while(myQueue.size() > 1) {
+    //3、开始数数字，不是就加入到末尾，是则取出
+    //num之前的人直接放到后面
+    for (let i = 0; i < num - 1; i++) {
+     myQueue.enqueue(myQueue.dequeue())
+    }
+    myQueue.dequeue()
+  }
+  //4、获取胜利者
+  console.log(myQueue.front)
+  return nameList.indexOf(myQueue.front() + 1)
+}
+console.log(passGame(['Ariel', 'Brant', 'Caven', 'Daisy', 'Eva'], 3))
