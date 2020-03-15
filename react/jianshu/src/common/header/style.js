c34cff12fb2400b9db0fa0731ecc0caff9359590
Brant-Liang <1351805238@qq.com>
2020-03-15 20:18:09 +0800

    jianshu

--- react/jianshu/src/common/header/style.js
+++ react/jianshu/src/common/header/style.js
@@ -21,5 +21,62 @@ export const Nav = styled.div`
   width: 960px;
   margin: 0 auto;
   height: 100%;
-  background-color: red;
+  padding-right: 70px;
+  box-sizing: border-box; 
+`
+export const NavItem = styled.div`
+  line-height: 65px;
+  padding: 0 15px;
+  font-size: 17px;
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+    color: #969696;
+  }
+  &.active {
+    color: #ea5f4a;
+  }
+`
+export const NavSearch = styled.input.attrs({
+  placeholder: '搜索'
+})`
+  width: 160px;
+  height: 38px;
+  border: none;
+  border-radius: 19px;
+  background-color: #eee;
+  margin-top: 9px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  font-size: 15px;
+  margin-left: 20px;
+  outline: none;
+  &::placeholder {
+    color: #999;
+  }
+`
+export const Addition = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 56px;
+`
+export const Button = styled.div`
+  float: right;
+  line-height: 38px;
+  border-radius: 19px;
+  margin-top: 9px;
+  margin-right: 20px;
+  padding: 0 20px;
+  border: 1px solid rgba(236, 97, 73, .7);
+  font-size: 14px;
+  &.reg {
+    color: #ec6149;
+  }
+  &.writting {
+    color: #fff;
+    background-color: #ec6149;
+  }
 `
