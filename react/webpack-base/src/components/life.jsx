da1a717ca537bcdb986a5815724b4a479472074e
Brant-Liang <1351805238@qq.com>
2020-03-18 00:08:04 +0800

    react life

--- react/webpack-base/src/components/life.jsx
+++ react/webpack-base/src/components/life.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ReactDOM from 'react-dom'
 export default class Life extends React.Component {
   constructor(props) {
     super(props)
@@ -8,12 +8,35 @@ export default class Life extends React.Component {
       opacity: 1
     }
   }
+  componentWillMount() {
+    console.log('willMount');
+  }
+  componentDidMount() {
+    //启动循环定时器
+    console.log('DidMount');
+    let { opacity } = this.state
+    this.timer =  setInterval(function() {
+      console.log('定时器执行');
+      opacity -= 0.1
+      if(opacity <= 0) {
+        opacity = 1
+      }
+      this.setState({opacity})
+    }.bind(this), 200)
+  }
+  componentWillUnmount() {
+    //清理定时器
+    clearInterval(this.timer)
+  }
+  distoryComponent() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('app'))
+  }
   render() {
-    const { opacity } = this.setState
+    const { opacity } = this.state
     return (
       <div>
         <h2 style={{opacity: opacity}}>{this.props.msg}</h2>
-        <button>不活了</button>
+        <button onClick={this.distoryComponent}>不活了</button>
       </div>
     )
   }
