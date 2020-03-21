bc74fbd5777daa41fb12ccd0203dca7aeff21797
Brant-Liang <1351805238@qq.com>
2020-03-22 00:38:06 +0800

    mappage dev

--- mpvue-koa/netease-shop/src/pages/index/index.vue
+++ mpvue-koa/netease-shop/src/pages/index/index.vue
@@ -1,5 +1,5 @@
- <template>
-   <div class="index">
+<template>
+  <div class="index">
      <!-- 头部搜搜 -->
      <div class="search">
        <div @click="toMappage">{{cityName}}</div>
@@ -9,24 +9,71 @@
        </div>
      </div>
    </div>
- </template>
- 
- <script>
- export default {
-   data() {
-     return {
-       cityName: '南昌'
-     }
-   },
-   methods: {
-     toMappage() {
-       
-     }
-   }
- }
- </script>
- 
+</template>
+
+<script>
+import amapFile from '@/utils/amap-wx'
+import { mapState, mapMutations } from 'vuex'
+export default {
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapState([
+      'cityName'
+    ])
+  },
+  mounted() {
+    this.getCityName() 
+  },
+  methods: {
+    ...mapMutations(['choiceAddress']),
+    toMappage() {
+      //通过wx.getSetting先查询用户是否授权 "scoped.record"
+      wx.getSetting({
+        success: ((res) => {
+          //如果没有
+          if(!res.authSetting['scope.userLocation']) {
+            wx.openSetting({
+              success: res => {
+                //获取授权位置信息
+                this.getCityName()
+              }
+            })
+          }
+          else {
+            wx.navigateTo({
+              url: '/pages/mappage/main'
+            })
+            // this.getCityName()
+          }
+        }),
+        fail: (err) => {
+          console.log(err);
+        },
+        complete: () => {}
+      })
+    },
+    getCityName() {
+      let _this = this
+      var myAmapFun = new amapFile.AMapWX({key:'fbcf5878a19e1bf2c09b12b6b4376645'});
+      myAmapFun.getRegeo({
+        success: function(data) {
+          //成功回调
+          console.log(data);
+          _this.choiceAddress(data[0].regeocodeData.addressComponent.city)
+        },
+        fail: function(info) {
+          console.log(info);
+          _this.choiceAddress('北京')
+        }
+      })
+    }
+  }
+}
+</script>
+
 <style lang="less" scoped>
- @import './style';
-</style>"
- 
+  @import "./style";
+</style>
---
+++ mpvue-koa/netease-shop/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+//安装插件
+Vue.use(Vuex)
+
+//创建对象
+const store = new Vuex.Store({
+  state: {
+    cityName: '定位中'
+  },
+  mutations: {
+    choiceAddress(state, payLoad) {
+      state.cityName = payLoad
+    },
+  }
+})
+
+export default store;
