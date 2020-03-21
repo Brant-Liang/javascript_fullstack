5bceb4077c114a1e3e65e700b7603b4a92c69924
Brant-Liang <1351805238@qq.com>
2020-03-21 14:44:04 +0800

    发布订阅者模式

--- interview/vue源码/Object.defineProperty/myVue.js
+++ interview/vue源码/Object.defineProperty/myVue.js
@@ -8,6 +8,10 @@ class myVue {
     this.$data = options.data
     console.log(options.data);
     this.observe(this.$data)
+
+    //模拟watcher创建
+    new Watcher()
+    this.$data.test;
   }
 
   observe(value) {
@@ -21,17 +25,47 @@ class myVue {
   defineReactive(obj, key, val){
 
     this.observe(val) //递归解决数据嵌套
-
+    const dep = new Dep()
     Object.defineProperty(obj, key, {
       get() {
+        this.Dep.target && dep.addDep(Dep.target)
         return val
       },
       set(newVal) {
         if(newVal !== val) {
           val = newVal
-          console.log(`${key}属性更新了：${val}`);
+          // console.log(`${key}属性更新了：${val}`);
+          dep.notify();
         }
       }
     })
+  }
+}
+
+//发布订阅者模式
+//Dep用来管理Watcher
+class Dep {
+  constructor() {
+    //存放若干依赖（watcher）
+    this.deps = []
+  }
+  addDep(dep) {
+    this.deps.push(dep)
+  }
+
+  notify() {
+    this.deps.forEach(dep => dep.update())
+  }
+}
+
+//Watcher
+
+class Watcher {
+  constructor() {
+    //将当前watcher实例指定到Dep静态属性target
+    Dep.target = this
+  }
+  update() {
+    console.log('属性更新了');
   }
 }
