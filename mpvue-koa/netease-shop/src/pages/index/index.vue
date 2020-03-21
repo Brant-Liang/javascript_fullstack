05e9d7f875f66f618f029d9ea36f52eac7954fd2
Brant-Liang <1351805238@qq.com>
2020-03-21 18:39:38 +0800

    netease shop

---
+++ mpvue-koa/netease-shop/src/pages/index/index.vue
@@ -0,0 +1,32 @@
+ <template>
+   <div class="index">
+     <!-- 头部搜搜 -->
+     <div class="search">
+       <div @click="toMappage">{{cityName}}</div>
+       <div>
+         <input type="text" placeholder="搜索商品">
+         <span class="icon"></span>
+       </div>
+     </div>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   data() {
+     return {
+       cityName: '南昌'
+     }
+   },
+   methods: {
+     toMappage() {
+       
+     }
+   }
+ }
+ </script>
+ 
+<style lang="less" scoped>
+ @import './style';
+</style>"
+ 
