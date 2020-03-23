354cd3a34cb0bf69fe58608d722ec3a662992bfa
Brant-Liang <1351805238@qq.com>
2020-03-23 23:35:36 +0800

    shop index

--- mpvue-koa/netease-shop/src/pages/index/index.vue
+++ mpvue-koa/netease-shop/src/pages/index/index.vue
@@ -24,7 +24,7 @@
        </div>
      </div>
      <div class="brand">
-       <div class="head">
+       <div class="head" @click="toBrandList">
          品牌制造商直供
        </div>
        <div class="content">
@@ -34,6 +34,89 @@
              <p>￥{{item.floor_price}}起</p>
            </div>
            <img :src="item.pic_url" alt="">
+         </div>
+       </div>
+     </div>
+     <div class="newgoods">
+       <div class="newgoods-top" @click="goodsList('new')">
+         <div class="top">
+           <p>新品首发</p>
+           <p>查看全部</p>
+         </div>
+       </div>
+       <div class="list">
+         <ul>
+           <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item, index) in newGoods" :key="index"> 
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>￥{{item.retail_price}}</p>
+            </li>
+           </scroll-view>
+         </ul>
+       </div>
+     </div>
+     <div class="newgoods hotgoods" @click="goodsList('hot')">
+       <div class="newgoods-top">
+         <div class="top">
+           <p>人气推荐
+             <span></span>
+             好物精选
+            </p>
+           <p>查看全部</p>
+         </div>
+       </div>
+       <div class="list">
+         <ul>
+           <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item, index) in hotGoods" :key="index"> 
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>￥{{item.retail_price}}</p>
+            </li>
+           </scroll-view>
+         </ul>
+       </div>
+     </div>
+     <div class="topicList">
+       <div class="topicList-top">
+         专题精选
+         <span class="icon"></span>
+       </div>
+       <div class="list">
+         <ul>
+           <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item, index) in topicList" @click="topicDetail(item.id)" :key="index">
+              <img :src="item.item_pic_url" alt="">
+              <div class="bottom">
+                <div>
+                  <p>{{item.title}}</p>
+                  <p>{{item.subtitle}}</p>
+                </div>
+                <div>{{item.price_info}}元起</div>
+              </div>
+            </li>
+         </scroll-view>
+         </ul>
+       </div>
+     </div>
+     <div class="newcategory">
+       <div class="list" v-for="(item, index) in newCategoryList" :key="index">
+         <div class="head">{{item.name}}好物</div>
+         <div class="sublist">
+           <div v-for="(subItem, subIndex) in item.goodList" :key="subIndex">
+             <img :src="subItem.list_pic_url" alt="">
+             <p>{{subItem.name}}</p>
+             <p>{{subItem.retail_price}}</p>
+           </div>
+           <div>
+             <div class="last">
+               <p>{{item.name}}好物</p>
+               <span class="icon"></span>
+             </div>
+           </div>
          </div>
        </div>
      </div>
@@ -49,7 +132,11 @@ export default {
     return {
       banner: [],
       channel: [],
-      brandList: []
+      brandList: [],
+      newGoods: [],
+      hotGoods: [],
+      topicList: [],
+      newCategoryList: []
     }
   },
   computed: {
@@ -110,15 +197,41 @@ export default {
       this.banner = data.banner
       this.channel = data.channel
       this.brandList = data.brandList
+      this.newGoods = data.newGoods
+      this.hotGoods = data.hotGoods
+      this.topicList = data.topicList
+      this.newCategoryList = data.newCategoryList
     },
     categoryList(url) {
       wx.navigateTo({
         url: url
       })
     },
+    toBrandList() {
+      wx.navigateTo({
+        url: '/pages/brandList/main'
+      })
+    },
     brandClick(id) {
       wx.navigateTo({
         url: '/pages/branddetail/main?id=' + id
+      })
+    },
+    goodsList(info) {
+      if(info === 'hot') {
+        wx.navigateTo({
+          url: '/pages/newgoods/main?isHot=' + 1
+        })
+      }
+      else if(info === 'new') {
+        wx.navigateTo({
+          url: '/pages/newgoods/main?isNew=' + 1
+        })
+      }
+    },
+    topicDetail(id) {
+      wx.navigateTo({
+        url: 'pages/topicdetail/main?id' + id
       })
     }
   }
