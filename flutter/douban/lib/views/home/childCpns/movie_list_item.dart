e68ec2a17fefb08ad364bd79c81d5a8c3f413d43
Brant-Liang <1351805238@qq.com>
2020-04-18 01:43:14 +0800

    ajax

--- flutter/douban/lib/views/home/childCpns/movie_list_item.dart
+++ flutter/douban/lib/views/home/childCpns/movie_list_item.dart
@@ -19,7 +19,7 @@ class MovieListItem extends StatelessWidget {
         children: <Widget>[
           getRankWidget(),
           SizedBox(height: 12,),
-          Text("2"),
+          getMovieContent(),
           SizedBox(height: 12,),
           getOriginWidget(),
         ],
@@ -38,7 +38,83 @@ class MovieListItem extends StatelessWidget {
         child: Text("No.${item.rank}",style: TextStyle(fontSize: 18,color: Color.fromARGB(255, 131, 95, 36)))
       );
   }
-  //2、
+  //2、获取内容显示
+  Widget getMovieContent() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        getMovieImage(),
+        getDetailDescWidget(),
+        getWishWidget (),
+      ],
+    );
+  }
+  //2.1获取图片
+  Widget getMovieImage() {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: Image.network(item.imageURL, height: 150,));
+  }
+  //2.2详情描述的Widget
+  Widget getDetailDescWidget(){
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            getMovieNameWidget(),
+            getRankingWidget(),
+            getIntroduceWidget()
+          ],
+        ),
+      ),
+    );
+  }
+  //2.2.1 获取电影名称的展示
+  Widget getMovieNameWidget() {
+    return Stack(
+      children: <Widget>[
+        Icon(Icons.play_circle_outline, color: Colors.red,size: 24,),
+        Text.rich(TextSpan(
+          children: [
+            TextSpan(text: "      " + item.title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            TextSpan(text: "(${item.playDate})")
+          ]
+        ))
+      ],
+    );
+  }
+  //2.2.2获取电影评分
+  Widget getRankingWidget() {
+    return Text("电影评分");
+  }
+  //2.3获取简介
+  Widget getIntroduceWidget() {
+    final genresString = item.genres.join(" ");
+    final director = item.director.name;
+    final actorString = item.casts.join(" ");
+    return Text(
+      "$genresString / $director / $actorString",
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(fontSize: 16),
+    );
+  }
+  //2.4想看的
+  Widget getWishWidget () {
+
+    return  Container(
+      padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+      child: Column(
+        children: <Widget>[
+          Image.asset("assets/images/home/wish.png",width: 32,),
+          SizedBox(height: 5,),
+          Text("想看", style: TextStyle(fontSize: 16,color: Color.fromARGB(255, 235, 170, 60)),)
+        ],
+      ),
+    );
+  }
   //3、获取原始电影名称的Widget
   Widget getOriginWidget() {
     return Container(
