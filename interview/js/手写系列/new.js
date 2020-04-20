683ef2ad657d82a152f8606205d818dcfd0f231a
Brant-Liang <1351805238@qq.com>
2020-04-20 17:51:29 +0800

    leetcode

--- interview/js/手写系列/new.js
+++ interview/js/手写系列/new.js
@@ -1,7 +1,13 @@
 function myNew(constructor, args) {
   var obj = {} //实例对象
-  obj.__proto__ == constructor.prototype
+  obj.__proto__ = constructor.prototype
   constructor.call(obj, ...args)
   
 }
-myNew(1,'2')
+myNew(Function,'2')
+
+function a() {
+  this.b = 1;
+}
+var obj = new a();
+console.log(obj.b);
