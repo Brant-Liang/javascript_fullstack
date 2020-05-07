7b0d1911116ca66cc077d656a998507809165a8f
Brant-Liang <1351805238@qq.com>
2020-05-07 21:08:55 +0800

    react

---
+++ react/todolist/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// PWA progress wab application 
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
