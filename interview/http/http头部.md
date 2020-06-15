43050d123ad3e14aa8a51416b7b917f30a6bb351
梁聪 <1351805238@qq.com>
2020-06-15 23:55:32 +0800

    interview

---
+++ interview/http/http头部.md
@@ -0,0 +1,7 @@
+Accept: 浏览器能够处理的内容类型
+Accept-Charset: 浏览器能够显示的字符集
+Accept-Encoding: 浏览器能够处理的压缩编码
+Accept-Language: 浏览器当前设置的语言。
+Connection: 浏览器与服务器之间连接的类型。
+Cookie: 当前页面设置的任何Cookie。
+Host: 发出请求的页面所在域。
--- interview/字节跳动前端面经/一面/数据检测.js
+++ interview/字节跳动前端面经/一面/数据检测.js
@@ -1,1 +1,27 @@
-console.log(typeof function(){});
+// console.log(typeof function(){});
+async function async1() {
+  await async2()
+  console.log('async1 end')
+}
+async function async2() {
+  console.log('async2 end')
+}
+console.log('script start')
+async1()
+
+setTimeout(function () {
+  console.log('setTimeout')
+}, 0)
+
+new Promise(resolve => {
+  console.log('Promise')
+  resolve()
+})
+  .then(function () {
+    console.log('promise1')
+  })
+  .then(function () {
+    console.log('promise2')
+  })
+
+console.log('script end')
