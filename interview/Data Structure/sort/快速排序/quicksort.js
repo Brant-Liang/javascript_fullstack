7001cbf9effd7f4df7c5ea7ae2f96f004ee2bb2f
梁聪 <1351805238@qq.com>
2020-07-08 22:35:43 +0800

    linux

--- interview/Data Structure/sort/快速排序/quicksort.js
+++ interview/Data Structure/sort/快速排序/quicksort.js
@@ -1,5 +1,5 @@
 function quickSort(arr, i, j) {
-  if (i >= j) return;
+  if (i > j) return;
   else {
     let left = i;
     let right = j;
@@ -44,4 +44,30 @@ function quickSort(arr, i, j) {
 
 // example
 let arr = [3, 1, 2, 0, 8, 6, 5, 9, 7, 4];
-console.log(quickSort(arr, 0, 9));
+let arr2 = [1]
+console.log(quickSort3(arr, 0, 9));
+
+function quickSort3(arr, i, j) {
+  if(i > j) return; 
+  let left = i;
+  let right = j;
+  let pivot = arr[left];
+  while(i < j) {
+    while(arr[j] >= pivot && i < j) {
+      j--;
+    }
+    if(i < j) {
+      arr[i++] = arr[j]
+    }
+    while(arr[i] <= pivot && i < j) {
+      i++;
+    }
+    if(i < j) {
+      arr[j--] = arr[i]
+    }
+  }
+  arr[i] = pivot;
+  quickSort3(arr, left, i - 1)
+  quickSort3(arr, i + 1, right)
+  return arr;
+}
