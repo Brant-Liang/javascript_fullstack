8483cebb94a5d8ac6fcb11ab1f43929fb63303ef
梁聪 <1351805238@qq.com>
2020-07-22 23:17:14 +0800

    react性能优化

--- react/Learn_React/learn-react-cli/src/index.js
+++ react/Learn_React/learn-react-cli/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom'
 // import App from './04_组件通信的案例/App'
 // import App from './05_react中使用插槽/App'
 // import App from './06_跨组件通信/App'
-import App from './07_setState的使用/App'
+// import App from './07_setState的使用/App'
+// import App from './08_React性能优化/03_PureComponent'
+import App from './08_React性能优化/04_memo的使用'
 
 ReactDOM.render(<App />, document.getElementById('root'))
