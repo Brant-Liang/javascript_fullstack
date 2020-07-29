e94e79fc515cedefd3980bd65725c5301eb856cb
梁聪 <1351805238@qq.com>
2020-07-30 00:09:17 +0800

    interview

--- interview/js/手写系列/instanceof.js
+++ interview/js/手写系列/instanceof.js
@@ -1,11 +1,20 @@
+// function instanceOf(left, right) {
+//   let proto = left.__proto__
+//   let prototype = right.prototype
+//   while(1) {
+//     if(proto === null) return false
+//     if(prototype === proto) return true
+//     proto = proto.__proto__
+//   }
+// }
+
 function instanceOf(left, right) {
-  let proto = left.__proto__
-  let prototype = right.prototype
+  let proto = left.__proto__;
+  let prototype = right.prototype;
   while(1) {
-    if(proto === null) return false
-    if(prototype === proto) return true
-    proto = proto.__proto__
+    if(proto === null) return false;
+    if(prototype === proto) return true;
+    proto = proto.__proto
   }
 }
-
 console.log(instanceOf(Object, Object));
