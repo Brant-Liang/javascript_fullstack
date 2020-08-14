0c2919e56b456fcbf06b468bf2169d65bb5fb843
梁聪 <1351805238@qq.com>
2020-08-14 23:55:16 +0800

    react hook useState

---
+++ react/Learn_React/12_react-hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import CounterClass from './01_体验hooks/01_counter-class'
+// import CounterHook from './01_体验hooks/02_counter-hook';
+import CounterHook from './01_体验hooks/03_counter-hook';
+
+export default function App() {
+  return (
+    <div>
+      {/* <CounterClass /> */}
+      <CounterHook />
+    </div>
+  )
+}
+
